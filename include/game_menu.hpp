bdfcb35c67229fc28f4e74cfc89725e7fc80929d
sebastian <sebastianschroder@hotmail.com>
2025-10-17 22:44:51 +1100

    moved menu structure to new structure and enabled better data collection through reflecting the menu structure into boxes. TODO find out how to correctly publish ansi escape characters

--- include/game_menu.hpp
+++ include/game_menu.hpp
@@ -5,7 +5,50 @@
 #include "ftxui/component/component.hpp"           // for Menu
 #include "ftxui/component/component_options.hpp"   // for MenuOption
 #include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
+#include "ftxui/dom/elements.hpp"                  // for color, bgcolor, etc.
+#include <ascii_generator.hpp>
+
 using namespace ftxui;
+
+
+
+struct MenuStructure{
+
+public:
+    Component Top_Component(){
+        return(top_level);
+    }    MenuStructure():
+    inventory_renderer(Renderer([this] { 
+        return paragraph(inventory_text) | reflect(inventory_box); 
+    })),
+    screen_renderer( Renderer([this] { 
+        return paragraph(screen_text) | reflect(screen_box); 
+    })),
+    action_renderer(Renderer([this] { 
+        return paragraph(action_text) | reflect(action_box); 
+    }))
+    {
+    top_level = screen_renderer;
+    top_level = ResizableSplitLeft(action_renderer,top_level,&left_size);
+    top_level = ResizableSplitBottom(inventory_renderer,top_level,&bottom_size);
+    }
+public:
+    Component inventory_renderer;
+    Box inventory_box;
+    Component screen_renderer;
+    Box screen_box;
+    Component action_renderer;
+    Box action_box;
+    Component top_level;
+public:
+std::string inventory_text = "inventory\n- items";
+std::string screen_text = "screen\n- images";
+std::string action_text = "action\n- menu";
+int left_size = 20;
+int bottom_size = 10;
+
+
+};
 
 class DisplayHUD{
 public:
@@ -13,21 +56,20 @@ public:
 
 
     void loop();
-
+    Element render();
+    void updateScreen();
 
 
 private:
 
-    std::vector<Component> components;
+    MenuStructure menu;
     ScreenInteractive screen;
     Component top_level_component;
     Component renderer;
+    AsciiGenerator generator;
     
     // Size variables for resizable splits (must be class members)
-    int left_size = 20;
-    int right_size = 20;
-    int top_size = 10;
-    int bottom_size = 10;
+
 
 };
 
--- src/game_menu.cpp
+++ src/game_menu.cpp
@@ -1,22 +1,60 @@
 #include <game_menu.hpp>
 
+#include <thread>
+#include <chrono>
+using namespace std::chrono_literals;
 
 DisplayHUD::DisplayHUD(): 
 screen(ScreenInteractive::Fullscreen())
 {
-    components.push_back( Renderer([] { return text("middle") | center; }));
-    components.push_back( Renderer([] { return text("Left") | center; }));
-    components.push_back( Renderer([] { return text("right") | center; }));
-    components.push_back( Renderer([] { return text("top") | center; }));
-    components.push_back( Renderer([] { return text("bottom") | center; }));
-    top_level_component = components.at(0);
-    top_level_component = ResizableSplitLeft(components.at(1), top_level_component, &left_size);
-    top_level_component = ResizableSplitRight(components.at(2), top_level_component, &right_size);
-    top_level_component = ResizableSplitTop(components.at(3), top_level_component, &top_size);
-    top_level_component = ResizableSplitBottom(components.at(4), top_level_component, &bottom_size);
-    renderer = Renderer(top_level_component,[&]{return top_level_component->Render()|border;});
+    top_level_component = menu.Top_Component();
+
+
+    renderer = Renderer(top_level_component,[this](){
+        return(this->render());
+    });
 }
 
 void DisplayHUD::loop(){
+    // Start a background thread to update the menu content
+    std::thread update_thread([this]() {this->updateScreen();});
+    // Detach the thread so it runs independently
+    update_thread.detach();
+    
+    // Start the main UI loop
     screen.Loop(renderer);
+}
+
+Element DisplayHUD::render(){
+    // Simply return the rendered component without any delays
+    //! Can add stuff here if necessary in the future.
+    return top_level_component->Render();
+}
+
+void DisplayHUD::updateScreen(){
+int i = 0;
+        while (true) {
+            std::this_thread::sleep_for(1000ms);
+            
+            // Update the menu text
+            menu.inventory_text = "inventory size : " 
+            + std::to_string(menu.inventory_box.y_max-menu.inventory_box.y_min)+"   " 
+            + std::to_string(menu.inventory_box.x_max-menu.inventory_box.x_min);
+            
+            menu.action_text = "action size : " 
+            + std::to_string(menu.action_box.y_max-menu.action_box.y_min)+"   " 
+            + std::to_string(menu.action_box.x_max-menu.action_box.x_min);
+
+
+            auto ascii = generator.generate_ascii_from_file("images/tree.jpg", menu.screen_box.x_max-menu.screen_box.x_min,menu.screen_box.y_max-menu.screen_box.y_min);
+            std::stringstream ss;
+            ss << ascii;
+            menu.screen_text = ss.str();
+
+
+            i++;
+            
+            // Trigger a screen refresh
+            screen.PostEvent(Event::Custom);
+        }
 }
